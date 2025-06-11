791625440ca0e436b597f383d0fc59e2eb7bf4a5
Mon <chavezt.ms@gmail.com>
2025-06-10 20:36:55 -0700

    terminado y excelente el ejercicio 9

--- Clase11/ejercicio9.js
+++ Clase11/ejercicio9.js
@@ -1,3 +1,14 @@
 /* Ejercicio 9: Cuenta regresiva para el despegue
 Consigna:
-Crea un programa que simule la cuenta regresiva para el lanzamiento de un cohete. El programa debe contar desde 10 hasta 0 e imprimir "¡Despegue!" al final. Usa un ciclo for para realizar la cuenta regresiva.*/
+Crea un programa que simule la cuenta regresiva para el lanzamiento de un cohete.
+ El programa debe contar desde 10 hasta 0 e imprimir "¡Despegue!" al final.
+  Usa un ciclo for para realizar la cuenta regresiva.*/
+
+  const prompt = require('prompt-sync')();
+
+ for (let num=10;num >=0 ;num --){
+    console.log ( num );
+
+
+ };
+ console.log ('¡Despegue!' );
