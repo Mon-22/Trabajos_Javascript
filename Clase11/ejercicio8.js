b0e8a7555258650356da05c62d93e92aa45cefee
Mon <chavezt.ms@gmail.com>
2025-06-10 20:53:51 -0700

    ejercicio terminado 8

--- Clase11/ejercicio8.js
+++ Clase11/ejercicio8.js
@@ -1,3 +1,12 @@
 /* Ejercicio 8: Encuentra los múltiplos de un número
 Consigna:
-Escribe un programa que le pida al usuario un número entero positivo y muestre todos los múltiplos de ese número entre 1 y 100. Utiliza un ciclo for para resolverlo.*/
+Escribe un programa que le pida al usuario un número entero positivo y muestre todos los múltiplos de ese número entre 1 y 100.
+' Utiliza un ciclo for para resolverlo.*/
+
+const prompt = require('prompt-sync')();
+
+ let numeroUsuario = parseFloat (prompt( 'Podrias por favor darme un numero entero positivo: ' ));
+
+ for (let num=numeroUsuario;num <=100 ;num +=numeroUsuario){
+    console.log ( num );
+ };
