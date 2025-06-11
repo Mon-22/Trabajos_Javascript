8306d422e6f0cb66ac6f3b5c7f3978447ded47a0
Mon <chavezt.ms@gmail.com>
2025-06-10 21:23:07 -0700

    ejercicio 6 resuelto

--- Clase11/ejercicio6.js
+++ Clase11/ejercicio6.js
@@ -3,3 +3,11 @@ Consigna:
 Crea un programa que le pida al usuario un número y 
 luego imprima su tabla de multiplicar desde el 1 hasta el 10. Usa un ciclo for.
 */
+
+const prompt = require('prompt-sync')();
+
+ let numeroUsuario = parseInt(prompt( 'Dame un numero  : ' ));
+
+ for (let num=1;num <=10 ;num++){
+    console.log (numeroUsuario + 'X' + num +'=' + (numeroUsuario*num)); 
+ };
