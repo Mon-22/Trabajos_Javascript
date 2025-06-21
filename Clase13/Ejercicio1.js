ab6d6476932b7d5266ceb7f8cd9ac75fa7797032
Mon <chavezt.ms@gmail.com>
2025-06-21 15:01:36 -0700

    por fin

---
+++ Clase13/Ejercicio1.js
@@ -0,0 +1,21 @@
+/* 
+Ejercicio 1: Área de un triángulo (función declarada) (Ejercicio entrevista)
+Crea una función declarada llamada calcularAreaTriangulo que reciba dos
+parámetros: la base y la altura de un triángulo. La función debe devolver el
+área del triángulo.
+Pista: Usa la fórmula:
+*/
+
+const prompt= require('prompt-sync')();
+
+function calcularAreaTriangulo (b,a) {
+    return (b*a)/2;
+}
+
+let b= parseFloat(prompt(' Escribe la base del triangulo, '));
+let a= parseFloat(prompt('Escribe la altura del triangulo: '));
+
+let calculo=calcularAreaTriangulo(a,b)
+
+
+console.log (' el area es: ' + calculo);
