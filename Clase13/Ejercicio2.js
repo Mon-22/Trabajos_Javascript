f3a93e1f11795be750f67b20187788d3b8591319
Mon <chavezt.ms@gmail.com>
2025-06-21 19:22:30 -0700

    usando un if ternario dentro de una funcion, esto es nuevo

---
+++ Clase13/Ejercicio2.js
@@ -0,0 +1,22 @@
+/*
+Ejercicio 2: Número mayor (función expresada)
+Crea una función expresada llamada encontrarMayor que reciba dos
+números como parámetros y devuelva el número mayor.
+*/
+
+const prompt = require('prompt-sync')();
+
+// funcion expresada
+
+
+const encontrarMayor = function (a,b){
+    return  a>b ? a:b; // puede ser con un if normal , en este caso utilice un operador ternario.
+}
+
+
+let a = parseFloat(prompt(' Dame un numero: '));
+let b = parseFloat(prompt(' Dame otro numero: '));
+
+let resultado = encontrarMayor(a,b);
+console.log( 'el numero mayor es:  ' + resultado);
+ 
